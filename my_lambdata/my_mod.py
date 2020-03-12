64fd73b38dfa152aff5345e784a19e06cb5b98f8
dataabyss <dataabyss@users.noreply.github.com>
2020-03-11 20:46:29 -0700

    added functions

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,6 +1,30 @@
 
 # my_lambdata/my_mod.py
 
+# 1) Train/val/test split for a df
+# 2) Split MM/DD/YYYY into multiple columns
+
+# Enter target in quotes
+def split(df, target):
+    from sklearn.model_selection import train_test_split
+
+    train, val = train_test_split(df, train_size=0.80, test_size=0.20,
+                    stratify=df[target],
+                    random_state=42)
+
+    val, test = train_test_split(val, train_size=0.80, test_size=0.20,
+                    stratify=val[target],
+                    random_state=42)
+
+    return train.shape, val.shape, test.shape
+
+def nans(df):
+    df.fillna(999, inplace=True)
+    return df.isna().sum()
+
+
+
+
 def enlarge(n):
     return n * 100
 
