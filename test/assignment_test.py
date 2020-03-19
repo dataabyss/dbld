5666bc68f2bf62345019bd48d73fcf603806ba3e
dataabyss <dataabyss@users.noreply.github.com>
2020-03-18 20:15:47 -0700

    legit assignement test

--- test/assignment_test.py
+++ test/assignment_test.py
@@ -1,4 +1,4 @@
-# test/my_mod_test.py
+# test/assignment_test.py
 
 import unittest
 
@@ -7,13 +7,19 @@ from my_lambdata.assignment import CustomFrame
 class TestCustomFrame(unittest.TestCase):
 
     def test_add_state_names(self):
-
         custom_df = CustomFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
+        # assert "name" should be a column
 
-        breakpoint()
-
+        self.assertTrue("name" not in list(custom_df.columns))
         custom_df.add_state_names()
-        #self.assertEqual(True, True)
+
+        # assert "name" should be a column
+        self.assertTrue("name" in list(custom_df.columns))
+
+        # assert "California" should exist in a column called "name"
+        # assert that first row, name is "Cali" and abbrev is "CA"
+        self.assertEqual(custom_df["abbrev"][0], "CA")
+        self.assertEqual(custom_df["name"][0], "Cali")
         
 if __name__ == '__main__':
     unittest.main()
